adbdbcf8f4550cc9ac69fc3ddafbab766e01f125
gamebox777 <rin7studio@gmail.com>
2020-10-02 21:49:56 +0900

    コミット

---
+++ gamebox_experiment/Assets/Gizmos/Gizmos00/Gizmos00.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Gizmos00 : MonoBehaviour
+{
+    public Transform CubePlayerTransform;
+
+    public Transform CubeSubTransform;
+
+    [SerializeField]
+    private GUIStyle gUIStyle;
+    
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+        Handles.color = Color.yellow;
+        Handles.DrawLine( CubePlayerTransform.position , CubeSubTransform.position);
+    }
+#endif    
+}

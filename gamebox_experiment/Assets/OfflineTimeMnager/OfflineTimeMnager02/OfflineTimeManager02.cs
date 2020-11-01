50d363f2e1c45a017e591dc3354b95fc0a7489b3
gamebox777 <rin7studio@gmail.com>
2020-11-01 12:47:02 +0900

    オフライン資源獲得その2

---
+++ gamebox_experiment/Assets/OfflineTimeMnager/OfflineTimeMnager02/OfflineTimeManager02.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// オフライン時の経過時間テスト
+/// 2020/10/27:ver1.0
+/// todo:OnApplicationPause,OnApplicationFocus時の処理は行っていません
+/// </summary>
+public class OfflineTimeManager02 : MonoBehaviour
+{
+    public static OfflineTimeManager02 instance;
+    
+    /// <summary> PlayerPrefsのセーブキー </summary>
+    const string SaveKeyString = "OfflineTimeMnager01SaveKey";
+    
+    /// <summary> 使用するクラス </summary>
+    [HideInInspector]
+    public ClassSaveData classSaveData;
+
+    /// <summary> 前回辞めた時間(セーブしていた時間) </summary>
+    [HideInInspector]
+    public DateTime oldDateTime;
+
+    /// <summary> 経過時間 </summary>
+    public float ElapsedTimeInSeconds { get; set; }
+    
+    private void Awake()
+    {
+        //シングルトン
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+        
+        SaveDataLoad();    //セーブデータをロードする
+        CalculateOfflineEarnings();    // オフライン時の経過時間を計算
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    
+    /// <summary>
+    /// オフライン時の経過時間を計算
+    /// </summary>
+    public void CalculateOfflineEarnings()
+    {
+        DateTime currentDateTime = DateTime.Now;
+        if (oldDateTime > currentDateTime) {
+            //データ不正：セーブデータの時間の方が今の時間よりも進んでいたので今の時間を入れる
+            oldDateTime = DateTime.Now;
+        }
+        
+        //アプリを停止→再開していた経過時間計算
+        TimeSpan timeElasped = currentDateTime - oldDateTime;    //経過した時間
+        ElapsedTimeInSeconds = (int)Math.Round(timeElasped.TotalSeconds, 0, MidpointRounding.ToEven); //経過時間
+        Debug.Log($"オフラインでの経過時間:{ElapsedTimeInSeconds}秒");
+    }
+    
+    /// <summary>
+    /// アプリケーションが終了された時に呼ばれる
+    /// </summary>
+    private void OnApplicationQuit()
+    {
+        Debug.Log("アプリが中断されたのでセーブ");
+        SaveDataSave();
+    }
+
+    /// <summary>
+    /// セーブデータ：ロード
+    /// </summary>
+    public void SaveDataLoad()
+    {
+        //セーブデータ存在チェック
+        if (PlayerPrefsJsonUtil.ExistData(SaveKeyString)) {
+            //セーブデータ有り
+            classSaveData = PlayerPrefsJsonUtil.GetObject<ClassSaveData>( SaveKeyString );
+            oldDateTime = classSaveData.GetDateTime();
+            string str = oldDateTime.ToString("yyyy/MM/dd HH:mm:ss");
+            Debug.Log($"アプリ開始時：セーブされていた時間:{str}");
+            
+            str = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+            Debug.Log($"今の時間:{str}");
+        }
+        else {
+            //セーブデータ無し
+            Debug.Log("セーブデータがないのでセーブデータ作成");
+            classSaveData = CreateSaveData();
+            oldDateTime = DateTime.Now;
+        }
+    }
+
+    /// <summary>
+    /// セーブデータ：セーブ
+    /// </summary>
+    public void SaveDataSave()
+    {
+        //デバッグ表示
+        string str = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+        Debug.Log($"アプリ終了時：セーブ時の時間:{str}");
+
+        //DateTime型をString型に変換してJSONで保存
+        classSaveData.dateTimeString = DateTime.Now.ToBinary().ToString();
+        PlayerPrefsJsonUtil.SetObject<ClassSaveData>(SaveKeyString,classSaveData,true);
+    }
+    
+    /// <summary>
+    /// セーブデータ作成
+    /// </summary>
+    /// <returns></returns>
+    public ClassSaveData CreateSaveData()
+    {
+        ClassSaveData saveData = new ClassSaveData
+        {
+            dateTimeString = DateTime.Now.ToBinary().ToString(),
+        };
+        return saveData;
+    }
+    
+    /// <summary>
+    /// セーブデータクラス
+    /// </summary>
+    [Serializable]
+    public struct ClassSaveData
+    {
+        /// <summary> DateTime型はシリアライズ出来ないので文字列型 </summary>
+        public string dateTimeString;
+
+        /// <summary>
+        /// DateTimeを文字列型で保存しているので、DateTime型に戻して取得
+        /// </summary>
+        /// <returns></returns>
+        public DateTime GetDateTime()
+        {
+            DateTime datetime = System.DateTime.FromBinary (System.Convert.ToInt64 (dateTimeString));
+            return datetime;
+        }
+    }
+    
+}
+
---
+++ gamebox_experiment/Assets/OfflineTimeMnager/OfflineTimeMnager02/OnlineManager.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class OnlineManager : MonoBehaviour
+{
+    [Serializable]
+    public class _BaseSettings {
+        /// <summary>
+        /// 貯まる資源の最大数
+        /// </summary>
+        //[Tooltip("Maximum number of lives by default for all players.")]
+        [CustomLabel("資源最大数")]
+        public int MaxResource = 999;
+
+        /// <summary>
+        /// Time to recover one life in minutes.
+        /// 1つの生命を回復する時間
+        /// </summary>
+        [CustomLabel("資源を1獲得する時間(秒)")]
+        public double SecondsToAcquired = 5d;
+    }
+    public _BaseSettings BaseSettings = new _BaseSettings();
+
+    public Text TextResource;
+    public Text TextTimer;
+    
+    /// <summary>
+    /// 現在の資源数
+    /// </summary>
+    private int mCurrentResources;
+    
+    /// <summary>
+    /// 次の資源を獲得するまでのタイマー(加算)
+    /// </summary>
+    private double mSecondsToNextResourceTimer;
+    
+    /// <summary>
+    /// todo:OfflineTimeManager02.csのawakeでオフライン経過時間を出してiいる
+    /// </summary>
+    private void Start()
+    {
+        mCurrentResources = 0;
+        mSecondsToNextResourceTimer = 0d;
+        SaveDataLoad();    //セーブデータをロードする
+        CalculateOfflineEarnings();    //オフライン経過分を計算
+    }
+
+    /// <summary>
+    /// オフライン時の経過時間から獲得資源を計算
+    /// </summary>
+    private void CalculateOfflineEarnings()
+    {
+        //オフライン時の経過時間取得(秒)
+        double elapsedTime = OfflineTimeManager02.instance.ElapsedTimeInSeconds;
+        Debug.Log($"OnlineManager経過時間:{elapsedTime}");
+
+        //資源獲得数
+        int GetResourceNum = (int)(elapsedTime / BaseSettings.SecondsToAcquired);
+        Debug.Log($"オフライン獲得資源数:{GetResourceNum}");
+
+        mCurrentResources = classSaveData.SaveResources + GetResourceNum;
+        
+        //獲得数に達さなかった時間を経過時間に足す
+        mSecondsToNextResourceTimer = elapsedTime - (BaseSettings.SecondsToAcquired * GetResourceNum );
+        Debug.Log($"資源獲得に達さなくて余った時間:{mSecondsToNextResourceTimer:F3}");
+
+        if ((mSecondsToNextResourceTimer + classSaveData.SecondsToNextResourceTimer) > BaseSettings.SecondsToAcquired) {
+            mCurrentResources += 1;
+            mSecondsToNextResourceTimer -= BaseSettings.SecondsToAcquired;
+        }
+    }
+
+    private void Update()
+    {
+        mSecondsToNextResourceTimer += Time.unscaledDeltaTime;
+        TextTimer.text = "資源が増えるtimer:" + mSecondsToNextResourceTimer.ToString();
+        TextResource.text = "資源数:" + mCurrentResources.ToString();
+        if (mSecondsToNextResourceTimer > BaseSettings.SecondsToAcquired) {
+            mCurrentResources += 1;
+            mSecondsToNextResourceTimer = 0;
+        }
+
+        //セーブデータの値更新 ※保存はしていない
+        classSaveData.SaveResources = mCurrentResources;
+        classSaveData.SecondsToNextResourceTimer = mSecondsToNextResourceTimer;
+    }
+    
+    //--------------------------------------------------------------------------
+    //
+    // セーブデータ部分
+    //
+    //--------------------------------------------------------------------------
+    
+    /// <summary> PlayerPrefsのセーブキー </summary>
+    const string SaveKeyString = "OnlineManager00";
+    
+    /// <summary> 使用するクラス </summary>
+    [HideInInspector]
+    public ClassSaveData classSaveData;
+
+    /// <summary>
+    /// アプリケーションが終了された時に呼ばれる
+    /// </summary>
+    private void OnApplicationQuit()
+    {
+        Debug.Log("アプリが中断されたのでセーブ");
+        SaveDataSave();
+    }
+
+    /// <summary>
+    /// セーブデータ：ロード
+    /// </summary>
+    public void SaveDataLoad()
+    {
+        //セーブデータ存在チェック
+        if (PlayerPrefsJsonUtil.ExistData(SaveKeyString)) {
+            //セーブデータ有り
+            classSaveData = PlayerPrefsJsonUtil.GetObject<ClassSaveData>( SaveKeyString );
+        }
+        else {
+            //セーブデータ無し
+            Debug.Log("セーブデータがないのでセーブデータ作成");
+            classSaveData = CreateSaveData();
+            classSaveData.SaveResources = 0;
+            classSaveData.SecondsToNextResourceTimer = 0d;
+        }
+    }
+
+    /// <summary>
+    /// セーブデータ：セーブ
+    /// </summary>
+    public void SaveDataSave()
+    {
+        PlayerPrefsJsonUtil.SetObject<ClassSaveData>(SaveKeyString,classSaveData,true);
+    }
+    
+    /// <summary>
+    /// セーブデータ作成
+    /// </summary>
+    /// <returns></returns>
+    public ClassSaveData CreateSaveData()
+    {
+        ClassSaveData saveData = new ClassSaveData
+        {
+            SaveResources = 0,
+            SecondsToNextResourceTimer = 0d,
+        };
+        return saveData;
+    }
+    
+    /// <summary>
+    /// セーブデータクラス
+    /// </summary>
+    [Serializable]
+    public struct ClassSaveData
+    {
+        public int SaveResources;
+        public double SecondsToNextResourceTimer;
+    }
+}
